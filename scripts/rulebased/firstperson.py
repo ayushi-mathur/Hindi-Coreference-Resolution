a47009ed742c754e296badb6612680226572cbc7
ayushi-mathur <ayushimathur1810@gmail.com>
2018-08-01 17:45:43 +0530

    First person pronoun resolution

---
+++ scripts/rulebased/firstperson.py
@@ -0,0 +1,19 @@
+complementizer = 'कि'
+
+import ssfAPI_intra as ssf
+
+def firstperson(mention):
+    #mention is a ssf.Node_vandan object
+    node = mention  #start at mention node
+    while (node.name != complementizer) and (node.parent != node):
+        node = node.parent
+    if node.parent == node:
+        return None
+    while (node.morphPOS != 'v'):
+        node = node.parent
+    cList = node.childList
+    for c in cList:
+        if c.parentRelation == 'k1':
+            answer = c
+            return answer
+    return None
