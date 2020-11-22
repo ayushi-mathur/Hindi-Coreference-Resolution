a9ff32847b06445baa8d0c221c2e77e8366a1544
ayushi-mathur <ayushimathur1810@gmail.com>
2020-11-22 12:22:21 +0530

    Updated sentence traversal checks

--- scripts/rulebased/locative.py
+++ scripts/rulebased/locative.py
@@ -5,11 +5,16 @@ def locative(mention, linearChunkList):  # isnt mention useless then?
     nChunk = linearChunkList[-1]  # the last chunk is the one we are on
     answer = None
     sentencesTraversed = 0
+    oldSentence = nChunk.upper
     i = len(linearChunkList)-2
-    while i >= 0 and sentencesTraversed <= 3:
-        if (linearChunkList[i].upper != nChunk.upper):
-            sentencesTraversed += 1
+    while i >= 0 and sentencesTraversed <= 2:
         nChunk = linearChunkList[i]
+        newSentence = nChunk.upper
+        if (newSentence is not oldSentence):
+            sentencesTraversed += 1
+            if (sentencesTraversed > 2):
+                return answer
+        oldSentence = newSentence
         
         if ('NP' in nChunk.name) and (nChunk.parentRelation in ['k7p', 'k2p']):    
             for c in nChunk.nodeList:
